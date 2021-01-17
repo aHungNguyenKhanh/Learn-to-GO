6d42dabe832bbd522cb35f9c26ca08250396bf82
aHungNguyenKhanh <hungnk1995@gmail.com>
2021-01-17 18:33:22 +0900

    Add flow control to model and REST API template to controller

--- simple-webapp/controllers/user.go
+++ simple-webapp/controllers/user.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"regexp"
+	"simple-webapp/models"
 )
 
 type userController struct {
@@ -11,6 +12,30 @@ type userController struct {
 
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from userController"))
+}
+
+func (uc userController) getALL(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+func (uc userController) get(id int, w http.ResponseWriter) {
+	//TODO
+}
+
+func (uc userController) post(w http.ResponseWriter) {
+	//TODO
+}
+
+func (uc userController) put(w http.ResponseWriter, r *http.Request) {
+	//TODO
+}
+
+func (uc userController) delete(id int, w http.ResponseWriter) {
+	//TODO
+}
+
+func (uc userController) parseRequest(r *http.Request) (models.User, error) {
+	//TODO
 }
 
 func newUserController() *userController {
